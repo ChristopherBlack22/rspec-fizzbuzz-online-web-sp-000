4751024ee6c7c28c7d225951eb7ebfc8bcec00fd
Christopher Black <christopher.black.2209@googlemail.com>
2021-03-20 21:12:10 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0
-    puts "Fizz"
+    "Fizz"
   elsif int % 5 == 0
     puts "Buzz"
   else int % 3 == 0 && int % 5 == 0
